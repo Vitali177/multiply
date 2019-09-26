303fbef9403c83098602606049f4eb9637f71c56
Vitali <vskrabatun@gmail.com>
2019-09-26 16:37:34 +0300

    refactor: delete change prototype String method, add new function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 module.exports = function multiply(first, second) {
 
-  String.prototype.splice = function(start, delCount, newSubStr) {
-    return this.slice(0, start) + newSubStr + this.slice(start + Math.abs(delCount));
-  };
-  
+  function spliceString(start, delCount, newSubStr, string) {
+    return string.slice(0, start) + newSubStr + string.slice(start + Math.abs(delCount));
+  }
+    
   // Каждую цифру бОльшего числа будем умножать на каждую цифру меньшего числа 
   // Первое число будет больше либо таким же  
-
-
   
   if ( ( second * 1 ) > ( first * 1 ) ) {
     var buffer = second;
@@ -26,19 +24,12 @@ module.exports = function multiply(first, second) {
   for (iSmallerNumber; iSmallerNumber >= 0; iSmallerNumber--) {
     for (iBiggerNumber; iBiggerNumber >= 0; iBiggerNumber--) {
 
-      if (result[iPositionForResult] !== undefined) { // Если на этом месте, есть уже цифра у результата, то прибавляем и его
+      if (result[iPositionForResult] !== undefined) // Если на этом месте, есть уже цифра у результата, то прибавляем и его
         thisNumber = ( first[iBiggerNumber] * second[iSmallerNumber] ) + ( result[iPositionForResult] * 1 ); 
-      } else {
-        thisNumber = first[iBiggerNumber] * second[iSmallerNumber]; 
-      }
-
-      //console.log('iPositionForResult -  ' + iPositionForResult);
-      //console.log('thisNumber - ' + thisNumber);
-
+      else thisNumber = first[iBiggerNumber] * second[iSmallerNumber];       
 
       if (thisNumber < 10) {
-        result = result.splice(iPositionForResult, 1, (thisNumber + '') );
-        //console.log('result - ' + result);
+        result = spliceString(iPositionForResult, 1, (thisNumber + ''), result);
         iPositionForResult++;
         continue;
       }
@@ -46,36 +37,22 @@ module.exports = function multiply(first, second) {
       // Если будет несколько раз подряд числа 10+ и мы должны будем двигаться по разрядам, то надо менять индекс для результата, чтобы правильно вписывать цифры в него
       iBuff = iPositionForResult;      
       while (thisNumber >= 10) {      
-
-        result = result.splice(iBuff, 1, (( thisNumber % 10 ) + '') );  // Переписываем эту цифру, на остаток 
+        result = spliceString(iBuff, 1, (( thisNumber % 10 ) + ''), result);  // Переписываем эту цифру, на остаток 
         iBuff++;   // и двигаемся к следующей разряду
 
-        if (result[iBuff] !== undefined) {  // Оставляем целую часть и прибавляем цифру этого разряда
+        if (result[iBuff] !== undefined)  // Оставляем целую часть и прибавляем цифру этого разряда
           thisNumber = Math.floor(thisNumber / 10.0) + ( result[iBuff] * 1); 
-        } else thisNumber = Math.floor(thisNumber / 10.0);         
+        else thisNumber = Math.floor(thisNumber / 10.0);       
 
-        //console.log("в цикле -  "  + thisNumber);
-
-        if (thisNumber < 10) {  // перезаписываем этот разряд, иначе возвращаемся в цикл и записываем уже только остаток от деления на 10
-         
-          result = result.splice(iBuff, 1, (thisNumber + ''));
-
-        }
+        if (thisNumber < 10)  // перезаписываем этот разряд, иначе возвращаемся в цикл и записываем уже только остаток от деления на 10      
+          result = spliceString(iBuff, 1, (thisNumber + ''), result);
+        
       }
       iPositionForResult++;
     }
     iBiggerNumber = first.length - 1;
-    iPositionForResult = second.length - iSmallerNumber;
-   
-    //console.log('iSmallerNumber -   ' +  iSmallerNumber);
-  }
-  
-
-  result = result.split('').reverse().join('');  
-  //console.log(result);
-
-
-
-  return result;
-  
+    iPositionForResult = second.length - iSmallerNumber;   
+  } 
+  result = result.split('').reverse().join('');    
+  return result;  
 }
